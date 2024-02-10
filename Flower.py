4f8203c3ff88bcd3fd127dfbeef7cc3287bdac49
jamesvillaraza <jamesvillaraza714@gmail.com>
2024-02-09 23:23:39 -0600

    comments on flower.py

--- Flower.py
+++ Flower.py
@@ -2,8 +2,8 @@
 
 class Flower():
     def __init__ (self, date, tasks, color):
-        dateAcquired = date
-        tasksCompleted = tasks
-        Flowercolor = color
+        dateAcquired = date #string
+        tasksCompleted = tasks #list of objects type Task
+        Flowercolor = color #string
 
         
