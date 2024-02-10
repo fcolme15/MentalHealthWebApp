05e5821d8edd4e111b794df1d68244a20f647a3c
Francisco Colmenero <franciscojcolmenero@outlook.com>
2024-02-10 01:20:02 -0600

    Fixed get task

--- Tasks.py
+++ Tasks.py
@@ -1,4 +1,5 @@
 from random import randint #Random num generator
+
 DrinkTask = [
     "Drink a glass of water",
     "Don't drink soda",
@@ -13,31 +14,15 @@ EatingTask = [
     "Plan your next meal",
     "Eat a cheat meal",
 ]
-
-journalTask = [
-    "Journal 3 good things that have happened today",
-    "Write down a short term or long term goal",
-    "Journal 3 things you are grateful for"
-]
-
-connectionTask = [
-    "Send a text or talk to someone in person",
-    "Leave a comment on someone's social media post"
-]
-
 class Tasks():
     def __init__ (self):
         self.randomNum = [] #Array of rand nums
         self.map = {
             1 : "DrinkTask",
             2 : "WalkingTask",
-            3 : "EatingTask",
-            4 : "journalTask",
-            5 : "connectionTask"
+            3 : "EatingTask"
         }
         
-
-
     def getRandom(self, loops = 5, highRange=8):
         for random in range (loops):
             result = randint(1,highRange) #random number 1-8
@@ -45,5 +30,6 @@ class Tasks():
 
     def GetTask(self):
         random = self.getRandom(1,len(self.map))
-        return random[self.getRandom(1,len(DrinkTask))]
+        random2 = self.getRandom(1,len(self.map[random]))
+        return self.map[random].index(random2)
 
---
+++ feed.py
@@ -0,0 +1,5 @@
+
+
+class Feed():
+    def __init__ (self):
+        self.something = "Just doing so i dont get error"
