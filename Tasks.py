1b21a0a0d4c5088b38a0a45e8319bc2961aaffb2
Francisco Colmenero <franciscojcolmenero@outlook.com>
2024-02-10 00:43:04 -0600

    GetTask func

--- Tasks.py
+++ Tasks.py
@@ -1,8 +1,27 @@
 from random import randint #Random num generator
-
+DrinkTask = [
+    "Drink a glass of water",
+    "Don't drink soda",
+]
+WalkingTask = [
+    "Take a 10 minute walk!",
+    "Take a 5 minute walk", 
+    "Take a 15 minute walk"
+]
+EatingTask = [
+    "Eat a fruit",
+    "Plan your next meal",
+    "Eat a cheat meal",
+]
 class Tasks():
     def __init__ (self):
         self.randomNum = [] #Array of rand nums
+        self.map = {
+            1 : "DrinkTask",
+            2 : "WalkingTask",
+            3 : "EatingTask"
+        }
+        
 
 
 
@@ -10,4 +29,8 @@ class Tasks():
         for random in range (loops):
             result = randint(1,highRange) #random number 1-8
             self.randomNum.append(result)
+
+    def GetTask(self):
+        random = self.getRandom(1,len(self.map))
+        return random[self.getRandom(1,len(DrinkTask))]
 
